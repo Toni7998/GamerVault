8c4818cae5a0331b274e96fe85bae763bb9e33f5
oriolcb <oriol.cantero@insbaixcamp.cat>
2025-05-06 16:35:52 +0200

    Correció mida imatges
    Mida de les imatges del logo corretgides.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,7 +20,7 @@
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <div>
             <a href="/">
-                <img src="{{ asset('media/logo-gamer_vault.png') }}" alt="Gamer Vault Logo" class="w-20 h-20">
+                <img src="{{ asset('media/logo-gamer_vault.png') }}" alt="Gamer Vault Logo">
             </a>
         </div>
 
